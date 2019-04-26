e2330b8c41b581d1e6c3f197a11fa44cb935085b
gochamog <siomeru@yahoo.co.jp>
2019-04-26 10:25:17 +0900

    20190426

--- client.c
+++ client.c
@@ -1,8 +1,21 @@
 #include<stdio.h>
 #include<arpa/inet.h>
 #include<sys/socket.h>
+#include<unistd.h>
+#include<string.h>
 
+void commun(int sock){
+	char buf[256];
+	int len_r;
+	char *message = "この授業が終わったら、俺……ガチャ回すんだ……！";
+	send(sock,message,strlen(message),0);
+	len_r = recv(sock,buf,256,0);
+	buf[len_r]='\n';
+	printf("%s\n",buf);
+	recv(sock,buf,256,0);
 	
+}
+
 int main(int argc,char**argv){
 	int sock = socket(PF_INET,SOCK_STREAM,0);
 	
@@ -15,6 +28,8 @@ int main(int argc,char**argv){
 	connect(sock,(struct sockaddr*)&target,sizeof(target));
 	
 	/*printf("sock is %d\n",sock);*/
+	
+	commun(sock);
 	
 	close(sock);
 	return 0;
